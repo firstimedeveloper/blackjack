c46e37ab00d3f71877e63c0d33ae4819d4dbe266
firstimedeveloper <hanjun.1998@gmail.com>
2018-05-23 03:42:10 +0900

    removed the outermost forloop in the StartGame function-- unnecessary. Also modified printing cards so that dealers last card is not visible.

--- blackjack/blackjack.go
+++ blackjack/blackjack.go
@@ -15,30 +15,34 @@ func StartGame(numOfPlayers int) {
 		fmt.Println(card)
 	}
 	getInput("Press enter to play blackjack")
-	gameOver := false
-	for !gameOver {
-		players := make([]Player, numOfPlayers)
-		var dealtCard deck.Card
-		for round := 0; round < 2; round++ {
-			for i := 0; i < numOfPlayers; i++ {
-				//dealtCard is the last Card in the deck
-				//which is the card that is being dealt
-				dealtCard = gameDeck[len(gameDeck)-1]
-				gameDeck = gameDeck[:len(gameDeck)-1]
-				//append dealtcard to hand
-				players[i] = append(players[i], dealtCard)
-				//players[0][0]
-				//players[1][0]
-			}
-		}
-		for i, player := range players {
-			for j, card := range player {
-				fmt.Printf("Player %d card %d: %v\n", i+1, j+1, card)
-			}
-		}
 
-		gameOver = true
+	players := make([]Player, numOfPlayers)
+	var dealtCard deck.Card
+	for round := 0; round < 2; round++ {
+		for i := 0; i < numOfPlayers; i++ {
+			//dealtCard is the last Card in the deck
+			//which is the card that is being dealt
+			dealtCard = gameDeck[len(gameDeck)-1]
+			gameDeck = gameDeck[:len(gameDeck)-1]
+			//append dealtcard to hand
+			players[i] = append(players[i], dealtCard)
+			//players[0][0]
+			//players[1][0]
+		}
 	}
+	for i, player := range players {
+		for j, card := range player {
+			if i+1 != len(players) || j != 1 {
+				if i+1 != len(players) {
+					fmt.Printf("Player %d card %d: %v\n", i+1, j+1, card)
+
+				} else {
+					fmt.Printf("Dealer's card %d: %v\n", j+1, card)
+				}
+			}
+		}
+	}
+
 }
 
 func getValue(c deck.Card) int {
