f7dd0ec2b39301ca89d282fb4ac5afaf4a47f2d7
firstimedeveloper <hanjun.1998@gmail.com>
2018-06-01 15:32:41 +0900

    now players are given the option to keep playing

--- main/main.go
+++ main/main.go
@@ -1,9 +1,27 @@
 package main
 
 import (
+	"fmt"
+	"os"
+	"strconv"
+
 	"github.com/firstimedeveloper/blackjack"
 )
 
 func main() {
-	blackjack.StartGame()
+	numOfPlayers, err := strconv.Atoi(blackjack.GetInput("Welcome to blackjack!\nEnter the number of players: "))
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(2)
+	}
+	gameOver := false
+	var input string
+	for !gameOver {
+		blackjack.StartGame(numOfPlayers)
+		input = blackjack.GetInput("Keep playing(y/n)? ")
+		if input == "n" {
+			gameOver = true
+			fmt.Println("Goodbye!")
+		}
+	}
 }
